27339270855ec3642960dda926e006a590b492f5
awesomerobot <kris.aubuchon@discourse.org>
2024-05-01 17:59:09 -0400

    REFACTOR: single simple mode, DTooltip, better sticky logic

--- javascripts/discourse/initializers/init-header-class.js
+++
@@ -1,6 +0,0 @@
-import { apiInitializer } from "discourse/lib/api";
-import HeaderTitleStateClass from "../components/header-title-state-class";
-
-export default apiInitializer("1.14.0", (api) => {
-  api.renderInOutlet("after-header", HeaderTitleStateClass);
-});
--- javascripts/discourse/services/sticky-map-state.js
+++ javascripts/discourse/services/sticky-map-state.js
@@ -3,41 +3,10 @@ import { action } from "@ember/object";
 import Service from "@ember/service";
 
 export default class StickyMapState extends Service {
-  @tracked
-  stickyMapVisible = settings.topic_map_type === "static bottom" ? true : false;
-  @tracked currentPost = null;
-
-  @tracked currentTab = null;
-
-  updateCurrentPost(post) {
-    return (this.currentPost = post);
-  }
-
-  updateCurrentTab(tab) {
-    if (tab === this.currentTab) {
-      return (this.currentTab = null);
-    }
-
-    return (this.currentTab = tab);
-  }
+  @tracked stickyMap = true;
 
   @action
   toggleStickyMap() {
-    if (settings.topic_map_type === "static bottom") {
-      return;
-    }
-
-    this.stickyMapVisible = !this.stickyMapVisible;
-
-    if (this.stickyMapVisible) {
-      // not a long term solution
-      // but moves focus for now
-
-      const mapBtn = document.querySelector(".sticky-topic-map .btn");
-
-      setTimeout(function () {
-        mapBtn?.focus();
-      }, 100);
-    }
+    this.stickyMap = !this.stickyMap;
   }
 }
