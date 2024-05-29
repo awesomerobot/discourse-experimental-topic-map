18b9db8e03b0ec22770b5474e1610e89e4012bc7
awesomerobot <kris.aubuchon@discourse.org>
2024-05-29 14:16:16 -0400

    DEV: only need to enable usercards for mobile

--- javascripts/discourse/initializers/init-menu-card-click.js
+++ javascripts/discourse/initializers/init-menu-card-click.js
@@ -2,14 +2,10 @@ import { apiInitializer } from "discourse/lib/api";
 
 export default apiInitializer("1.20.0", (api) => {
   const site = api.container.lookup("service:site");
-  // enables user cards in the users menu
-
-  if (document.querySelector(".topic-map.--simplified")) {
-    api.addCardClickListenerSelector(".topic-map.--simplified");
-  }
-
+  //  workaround for now
+  // enables user cards in the mobile users modal
+  // by allowing usercards in all mobile modals
   if (site.mobileView) {
-    // workaround for now
     api.addCardClickListenerSelector(".modal-container");
   }
 });
